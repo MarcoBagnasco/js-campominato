981910bc71a77cf491bd6809898db198358bcc36
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-06 16:30:12 +0200

    add setup variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('hello'); //test
+/* 
+CAMPO MINATO
+- Il computer deve generare 16 numeri casuali da 1 a 100 (bombe).
+- In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta, se il numero è presente nella lista dei numeri generati la partita termina altrimenti continua chiedendo all’utente un altro numero.
+- La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+- Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+*/
+
+// Setup
+var numMax = 10;
+var numBomb = 2;
+var attempt = numMax - numBomb;
+var bombList = [];
+var insertList = [];
+var user = 0;
